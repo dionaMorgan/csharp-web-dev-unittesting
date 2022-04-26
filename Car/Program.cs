c1436132f4141651124eb1d421d4e7da5bc6c3b9
Diona Morgan <diona.morgan@gmail.com>
2022-04-26 12:51:44 -0500

    Unit testing exercise

--- Car/Program.cs
+++ Car/Program.cs
@@ -8,6 +8,10 @@ namespace CarNS
         {
             Car car = new Car("Toyota", "Prius", 10, 50);
             Console.WriteLine(car.Make + " - " + car.Model);
+            Console.WriteLine(car.GasTankLevel);
+            car.Drive(100);
+            car.AddGas(5);
+            Console.WriteLine(car.GasTankLevel);
         }
     }
 }
