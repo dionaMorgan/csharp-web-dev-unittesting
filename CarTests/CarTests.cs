1280a6145c5aec82c7cb76f5ea0484a050129d4c
Diona Morgan <diona.morgan@gmail.com>
2022-04-27 13:54:55 -0500

    final

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -15,12 +15,6 @@ namespace CarTests
             test_car = new Car("Toyota", "Prius", 10, 50); //make, model, gasTankSize, milesPerGallon
         }
 
-        //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
-        [TestMethod]
-        public void EmptyTest()
-        {
-            Assert.AreEqual(10, 10, .001);
-        }
 
         //TODO: constructor sets gasTankLevel properly
         [TestMethod]
